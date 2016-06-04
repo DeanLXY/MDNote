68e2c8983240e77b05dbbb38d87e882691da194a
wangxujie <wangxj0714@163.com>
2016-06-05 00:57:07 +0800

    取消首页搜索

--- app/src/main/java/com/wxj/mdnote/SplashActivity.java
+++ app/src/main/java/com/wxj/mdnote/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.wxj.mdnote;
 
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Thread(new CheckTask()).start();
+    }
+
+
+    private class CheckTask implements Runnable{
+
+        @Override
+        public void run() {
+            SystemClock.sleep(2000);
+            startActivity(new Intent(getBaseContext(),NoteListActivity.class));
+            finish();
+        }
     }
 }
